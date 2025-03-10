9a73f0ec9415f2366a269159a844ca2c0a9ae58d
Steven Hahn <hahnse@ornl.gov>
2025-03-10 14:35:09 -0400

    optimization?
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

--- src/bixbyite_topaz.cpp
+++ src/bixbyite_topaz.cpp
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
 
   parameters params;
   params.axes =
-      std::tuple<reg, reg, reg>{reg(603, -16.0, 16.0, "x"), reg(603, -16.0, 16.0, "y"), reg(1, -0.1, 0.1, "z")};
+      std::tuple<reg, reg, reg>{reg(201, -16.0, 16.0, "x"), reg(201, -16.0, 16.0, "y"), reg(1, -0.1, 0.1, "z")};
   params.W << 1.f, 0.f, 0.f, 0.f, 1.f, 0.f, 0.f, 0.f, 1.f;
   params.solidAngleFilename = BIXBYITE_SA_NXS;
   params.fluxFilename = BIXBYITE_FLUX_NXS;
